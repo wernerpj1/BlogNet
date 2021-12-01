46606d6e4e6ada3360d5686d8ba2c3e58d664811
wernerpj1 <wernerpj1@gmail.com>
2021-12-01 08:21:13 -0300

    mappings2

--- back/Infrastructure/Data/Mappings/UsuarioMapping.cs
+++ back/Infrastructure/Data/Mappings/UsuarioMapping.cs
@@ -1,14 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using back.Business.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace back.Infrastructure.Data.Mappings
 {
     public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
     {
-        
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("TB_USUARIO");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+            builder.Property(p => p.Email);
+            builder.Property(p => p.Senha);
+            builder.Property(p => p.Active);
+            builder.Property(p => p.Excluido);
+
+        }
     }
 }
